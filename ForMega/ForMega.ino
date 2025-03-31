d297470b5a68326086ffe89f5aae7b9ddb4a5e28
tima03 <tima-psp2002@mail.ru>
2025-04-01 01:20:51 +0300

    v1.4 рабочая прокрутка меню, а так же самостоятельный выбор количества и названия элеметнов

--- ForMega/ForMega.ino
+++ ForMega/ForMega.ino
@@ -1,107 +1,99 @@
 #include <SimpleMenu.h>
-
 #include <U8g2lib.h>
 #include <GParser.h>
 #include "GyverEncoder.h"
-
-
 #include <Wire.h>
 
-#define PIN_LED 13  // вывод светодиода
+#define PIN_LED 13
 #define S2 18
 #define S1 19
 #define KEY 20
 #define OLEDSCL A0
 #define OLEDSDA A1
-#define MenuPointsNum 4
-
+#define MenuPointsNum 20
 
 String inString;
 Encoder enc(S1, S2, KEY); 
-
 U8G2_ST7567_ENH_DG128064I_F_SW_I2C oled(U8G2_R0, OLEDSCL, OLEDSDA, U8X8_PIN_NONE);
+simpleMenu* menu = nullptr;
 
-const menuStruct points [MenuPointsNum] PROGMEM= {
-	{"Меню", 0},
-	{"Реле", 1 },
-	{"Лит энерджи", 2}, 
-	{"Стрим ёлки", 3},  
+const menuStruct points [MenuPointsNum] PROGMEM = {
+    {"Главное меню", 0},     // Заголовок
+    {"Включить свет", 1},
+    {"Выключить свет", 2},
+    {"Заряд батареи", 3},
+    {"Температура", 4},
+    {"Влажность", 5},
+    {"Датчик движения", 6},
+    {"Настройки Wi-Fi", 7},
+    {"Обновить прошивку", 8},
+    {"Режим сна", 9},
+    {"Таймер", 10},
+    {"Яркость экрана", 11},
+    {"Звук вкл/выкл", 12},
+    {"Сброс настроек", 13},
+    {"Тест дисплея", 14},
+    {"Версия ПО", 15},
+    {"Лог событий", 16},
+    {"Калибровка", 17},
+    {"Режим отладки", 18},
+    {"Выход", 19},
 };
 
-
-
 void setup() {   
-  simpleMenu menu(points, oled, MenuPointsNum);
-  Serial.begin(115200);
-  Serial3.begin(115200);
-  Serial.setTimeout(5);
-  Serial3.setTimeout(5);
-  pinMode(PIN_LED, OUTPUT);
-  digitalWrite(PIN_LED, LOW);
-  enc.setType(TYPE2);
-  menu.DrawMenu();
+    Serial.begin(115200);
+    Serial3.begin(115200);
+    Serial.setTimeout(5);
+    Serial3.setTimeout(5);
+    pinMode(PIN_LED, OUTPUT);
+    digitalWrite(PIN_LED, LOW);
+    enc.setType(TYPE2);
     
+    Serial.println("Starting setup");
+    menu = new simpleMenu(points, oled, MenuPointsNum);
+    Serial.println("Menu created");
+    menu->DrawMenu();
+    Serial.println("Menu drawn");
 }
 
-// Выполнение
 void loop() {
-  //menu.control();
-  //enc.tick();
-
-  //if (enc.isRight()) Serial.println("Right");         // если был поворот
-  //if (enc.isLeft()) Serial.println("Left");
-  
-  //if (enc.isRightH()) Serial.println("Right holded"); // если было удержание + поворот
-  //if (enc.isLeftH()) Serial.println("Left holded");
-  
-  //if (enc1.isPress()) Serial.println("Press");         // нажатие на кнопку (+ дебаунс)
-  //if (enc1.isRelease()) Serial.println("Release");     // то же самое, что isClick
-  
-  //if (enc.isClick()) Serial.println("Click");         // одиночный клик
-  //if (enc.isSingle()) Serial.println("Single");       // одиночный клик (с таймаутом для двойного)
-  //if (enc.isDouble()) Serial.println("Double");       // двойной клик
-  
-  
-  //if (enc.isHolded()) Serial.println("Holded");       // если была удержана и энк не поворачивался
-  //if (enc1.isHold()) Serial.println("Hold");   
-
+    enc.tick();
+    bool right = enc.isRight();
+    bool left = enc.isLeft();
+    if (right || left) {
+        Serial.print("Detected - Right: ");
+        Serial.print(right);
+        Serial.print(", Left: ");
+        Serial.println(left);
+        menu->updateEncoder(right, left);
+    }
 }
 
-// Проверка события на порту Serial3
 void serialEvent3() {
-  while (Serial3.available()) {
-    // Чтение данных из порта Serial3
-    char str[30];
-    int amount = Serial3.readBytesUntil(';', str, 30);
-    str[amount] = NULL;
-
-    GParser data(str);
-    int datacount = data.split();
-    if (data[0] != NULL && datacount>=2) {
-      switch (data.getInt(0)) {
-        case 0: //вывод с новой строки
-          Serial.println(data[1]);
-          break;
-        case 1: //вывод подряд
-          Serial.print(data[1]);
-          break;
-        case 2: //отступ строки
-          Serial.println(" ");
-          break;
-        case 3: //Управление светодиодом
-          if(data.getInt(1)==1){
-            digitalWrite(PIN_LED, HIGH);
-            Serial.println("Светодиод включен");
-          }else{
-            digitalWrite(PIN_LED, LOW);
-            Serial.println("Светодиод выключен");
-          }
-          break;
-        default:
-          String strout(str);
-          Serial.println("Нет такого ключа: "+ strout);
-          // код для выполнения
-      }
+    while (Serial3.available()) {
+        char str[30];
+        int amount = Serial3.readBytesUntil(';', str, 30);
+        str[amount] = NULL;
+        GParser data(str);
+        int datacount = data.split();
+        if (data[0] != NULL && datacount >= 2) {
+            switch (data.getInt(0)) {
+                case 0: Serial.println(data[1]); break;
+                case 1: Serial.print(data[1]); break;
+                case 2: Serial.println(" "); break;
+                case 3:
+                    if (data.getInt(1) == 1) {
+                        digitalWrite(PIN_LED, HIGH);
+                        Serial.println("Светодиод включен");
+                    } else {
+                        digitalWrite(PIN_LED, LOW);
+                        Serial.println("Светодиод выключен");
+                    }
+                    break;
+                default:
+                    String strout(str);
+                    Serial.println("Нет такого ключа: " + strout);
+            }
+        }
     }
-  }
 }
--- SimpleMenu/SimpleMenu.h
+++ SimpleMenu/SimpleMenu.h
@@ -2,76 +2,146 @@
 
 #include <U8g2lib.h>
 #include <Arduino.h>
-//in this struct there are the point parameters 
+
 struct menuStruct {
     const char p_name[128];
     const int8_t val1;
 };
 
-
 class simpleMenu {
-private:                    //references
-
+private:
     const menuStruct* const _pCtrls;
     const int8_t _pNum;
     int8_t _pointer;
+    int8_t _encoderPos;
+    int8_t _currentPage;
+    int8_t _totalPages;
     char buffer[128];
     U8G2_ST7567_ENH_DG128064I_F_SW_I2C& _LCD;
 
 public:
-    
-    simpleMenu(const menuStruct* const pointCtrls, U8G2_ST7567_ENH_DG128064I_F_SW_I2C& objectLCD, const int8_t pointsNum):_pCtrls(pointCtrls), _LCD(objectLCD), _pNum(pointsNum) 
-    {   
+    simpleMenu(const menuStruct* const pointCtrls, U8G2_ST7567_ENH_DG128064I_F_SW_I2C& objectLCD, const int8_t pointsNum)
+        : _pCtrls(pointCtrls), _LCD(objectLCD), _pNum(pointsNum), _pointer(0), _encoderPos(0), _currentPage(0)
+    {
+        _totalPages = 1 + ((_pNum - 4) + 3) / 4;
         _LCD.setI2CAddress(0x3F * 2);
+        Serial.println("Initializing LCD");
         _LCD.begin();
-        _LCD.enableUTF8Print();  
-        _LCD.clearBuffer();  
-        _LCD.setDisplayRotation(U8G2_R2) ;  
-        _LCD.setContrast(200); 
+        Serial.println("LCD initialized");
+        _LCD.enableUTF8Print();
+        _LCD.setDisplayRotation(U8G2_R2);
+        _LCD.setContrast(200);
         _LCD.setFont(u8g2_font_6x12_t_cyrillic);
-        _pointer = 1;
-        //_LCD.drawRFrame(0,0,128,15,5);
-        //_LCD.drawUTF8(0, 10, "абвгдеёжзийклмнопрстуфхцчъыьэюя");
-        //_LCD.sendBuffer();
-        //objectLCD.drawRFrame(0,16,128,15,5);
-        //objectLCD.drawUTF8(0, 26, "абвгдеёжзийклмнопрстуфхцчъыьэюя");
-        //objectLCD.drawRFrame(0,32,128,15,5);
-        //objectLCD.drawUTF8(0, 42, "абвгдеёжзийклмнопрстуфхцчъыьэюя");
-        //objectLCD.drawRFrame(0,48,128,15,5);
-        //objectLCD.drawUTF8(0, 58, "абвгдеёжзийклмнопрстуфхцчъыьэюя");
-        
-        //firstLaunch();
-    }                   //public functions
+    }
 
-public:
-bool DrawMenu() {
-    if (_pNum < 5) {
-        for (int8_t i = 0; i < _pNum; i++) {
-            // Читаем val1 из PROGMEM
-            int8_t myVal = pgm_read_byte(&_pCtrls[i].val1);
-            // Копируем имя пункта из PROGMEM в буфер
-            strcpy_P(buffer, _pCtrls[i].p_name);
-            if (_pointer == 1){_LCD.drawRFrame(0, 16, 128, 15, 5);}
-            //_LCD.drawRFrame(0, 0, 128, 15, 5);
-            //_LCD.drawRFrame(0, 16, 128, 15, 5);
-            //_LCD.drawRFrame(0, 32, 128, 15, 5);
-            //_LCD.drawRFrame(0, 48, 128, 15, 5);
-            if (myVal == 0) {
-                // Если val1 == 0, это заголовок — центрируем
-                int8_t textLength = strlen(buffer)/4; // Длина строки в символах
-                int8_t textWidth = textLength * 6;  // Ширина текста в пикселях (6 — ширина символа)
-                int8_t x = 64 - textWidth;   // Центрируем: (ширина экрана - ширина текста) / 2
-                _LCD.drawUTF8(x, 10, buffer);       // Выводим заголовок
-            } else {
-                // Если val1 != 0, это пункт меню — рисуем рамку и текст
-                int8_t yPos = 10+16*i;
-                _LCD.drawUTF8(5, yPos, buffer);  
+    void updateEncoder(bool isRight, bool isLeft) {
+        Serial.print("Encoder update - Right: ");
+        Serial.print(isRight);
+        Serial.print(", Left: ");
+        Serial.println(isLeft);
+
+        if (isRight) {
+            _encoderPos++;
+            Serial.print("Encoder pos: ");
+            Serial.println(_encoderPos);
+            if (_encoderPos >= 2) {
+                int8_t maxItems = (_currentPage == 0) ? min(3, _pNum - 1) : min(4, _pNum - (_currentPage * 4));
+                if (_pointer < maxItems - 1) {
+                    _pointer++;
+                    Serial.print("Pointer incremented to: ");
+                    Serial.println(_pointer);
+                } else if (_currentPage < _totalPages - 1) {
+                    _currentPage++;
+                    _pointer = 0;
+                    Serial.print("Page incremented to: ");
+                    Serial.println(_currentPage);
+                } else {
+                    _currentPage = 0;
+                    _pointer = 0;
+                    Serial.println("Wrapped to first page, first item");
+                }
+                _encoderPos = 0;
+                DrawMenu();
+            }
+        }
+        if (isLeft) {
+            _encoderPos--;
+            Serial.print("Encoder pos: ");
+            Serial.println(_encoderPos);
+            if (_encoderPos <= -2) {
+                int8_t maxItems = (_currentPage == 0) ? min(3, _pNum - 1) : min(4, _pNum - (_currentPage * 4));
+                if (_pointer > 0) {
+                    _pointer--;
+                    Serial.print("Pointer decremented to: ");
+                    Serial.println(_pointer);
+                } else if (_currentPage > 0) {
+                    _currentPage--;
+                    _pointer = min(3, _pNum - (_currentPage * 4) - 1);
+                    Serial.print("Page decremented to: ");
+                    Serial.println(_currentPage);
+                } else {
+                    _currentPage = _totalPages - 1;
+                    _pointer = min(3, _pNum - (_currentPage * 4) - 1);
+                    Serial.print("Wrapped to last page, last item: ");
+                    Serial.println(_pointer);
+                }
+                _encoderPos = 0;
+                DrawMenu();
             }
         }
     }
-    _LCD.sendBuffer();
-    return true;
-}
 
+    bool DrawMenu() {
+        Serial.print("Drawing menu - Page: ");
+        Serial.print(_currentPage);
+        Serial.print(", Pointer: ");
+        Serial.println(_pointer);
+
+        _LCD.clearBuffer();
+
+        if (_currentPage == 0) {
+            strcpy_P(buffer, _pCtrls[0].p_name);
+            int8_t textLength = strlen(buffer);  // Точная длина строки в байтах
+            int8_t textWidth = textLength * 6 / 2;  // Ширина текста (6 пикселей на символ, делим на 2 для UTF-8)
+            int8_t x = (128 - textWidth) / 2;  // Центрирование: (ширина экрана - ширина текста) / 2
+            _LCD.drawUTF8(x, 10, buffer);  // Центрированный заголовок
+            Serial.println("Title centered");
+
+            for (int8_t i = 0; i < 3 && (i + 1) < _pNum; i++) {
+                strcpy_P(buffer, _pCtrls[i + 1].p_name);
+                int8_t yFrame = 16 * (i + 1);  // 16, 32, 48
+                int8_t yText = yFrame + 10;     // 26, 42, 58
+                if (i == _pointer) {
+                    _LCD.drawRFrame(0, yFrame, 128, 15, 5);  // Отступ 5 пикселей, ширина 118 (128 - 5 - 5)
+                    Serial.print("Frame drawn at item: ");
+                    Serial.println(i);
+                }
+                _LCD.drawUTF8(5, yText, buffer);  // Отступ 5 пикселей для текста
+            }
+        } else {
+            int8_t startIndex = 4 + (_currentPage - 1) * 4;
+            for (int8_t i = 0; i < 4 && (startIndex + i) < _pNum; i++) {
+                strcpy_P(buffer, _pCtrls[startIndex + i].p_name);
+                int8_t yFrame = 16 * i;      // 0, 16, 32, 48
+                int8_t yText = yFrame + 10;  // 10, 26, 42, 58
+                if (i == _pointer) {
+                    _LCD.drawRFrame(0, yFrame, 128, 15, 5);  // Отступ 5 пикселей, ширина 118
+                    Serial.print("Frame drawn at item: ");
+                    Serial.println(i);
+                }
+                _LCD.drawUTF8(5, yText, buffer);  // Отступ 5 пикселей для текста
+            }
+        }
+
+        _LCD.sendBuffer();
+        Serial.println("Buffer sent");
+        return true;
+    }
+
+    int8_t getPointer() {
+        if (_currentPage == 0) {
+            return _pointer + 1;
+        }
+        return 4 + (_currentPage - 1) * 4 + _pointer;
+    }
 };
-
